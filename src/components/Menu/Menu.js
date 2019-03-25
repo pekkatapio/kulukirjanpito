95e26c222b2509e5f2768f0963dca61a7ded9432
Pekka Tapio Aalto <pekka.aalto@sasky.fi>
2019-03-25 14:41:52 +0200

    Initial commit with basic template

---
+++ src/components/Menu/Menu.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import ViewList from '@material-ui/icons/ViewList';
+import TrendingUp from '@material-ui/icons/TrendingUp';
+import Settings from '@material-ui/icons/Settings';
+
+import './Menu.css';
+
+function Menu(props) {
+    return (
+      <div className="menu">
+        <Link to="/"><div className="menu__nappi"><ViewList nativeColor="#fff" /></div></Link>
+        <Link to="/stats"><div className="menu__nappi"><TrendingUp  nativeColor="#fff" /></div></Link>
+        <Link to="/settings"><div className="menu__nappi"><Settings  nativeColor="#fff" /></div></Link>
+      </div>
+    );
+}
+
+export default Menu;
